672703f44d3c605a952f18e91ed09e30ca04d281
banzayman <vanopadal@gmail.com>
2023-02-20 20:24:50 +0300

    bla

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use std::collections::HashMap;
+use std::fmt::LowerHex;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::ptr::read;
 use std::str::FromStr;
 use std::sync::{Arc, Mutex};
-use std::{io::*, thread, prelude::*};
+use std::{io::*, thread};
 use rand::Rng;
+
+pub fn put_name(gt_pt_dlt_obj_name: &Mutex<HashMap<String, String>>, new_id: String, nm: String){
+    gt_pt_dlt_obj_name.lock().unwrap().insert(new_id, nm);
+}
+
+
+fn dlt_name(gt_pt_dlt_obj_name: &Mutex<HashMap<String, String>>, id: &String){
+    gt_pt_dlt_obj_name.lock().unwrap().remove(id);
+}
+
+
+fn get_name(gt_pt_dlt_obj_name: &Mutex<HashMap<String, String>>, id: String){
+    //gt_pt_dlt_obj_name.lock().unwrap().remove(id);
+    gt_pt_dlt_obj_name.lock().unwrap().entry(id);
+    let k = 2.to_string();
+    let binding = gt_pt_dlt_obj_name.lock().unwrap();
+    let data = binding.get(&k);
+    println!("{:?}", data);
+}
+
 
 fn handle_client(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
@@ -21,37 +42,18 @@ fn handle_client(mut stream: TcpStream) {
     obj_name.lock().unwrap().insert(2.to_string(),"DS_H1312_0024_WORKCNTR".to_string());
     obj_name.lock().unwrap().insert(3.to_string(),"FU_H1312_0024_10010269_P".to_string());
 
-        
+    
     let put_obj_name = obj_name.clone();
     let dlt_obj_name = obj_name.clone();
     let get_obj_name = obj_name.clone();
     
-    fn put_name(gt_pt_dlt_obj_name: &Mutex<HashMap<String, String>>, new_id: String, nm: String){
-        gt_pt_dlt_obj_name.lock().unwrap().insert(new_id, nm);
-    }
-
- 
-    fn dlt_name(gt_pt_dlt_obj_name: &Mutex<HashMap<String, String>>, id: &String){
-        gt_pt_dlt_obj_name.lock().unwrap().remove(id);
-    }
-
-    fn get_name(gt_pt_dlt_obj_name: &Mutex<HashMap<String, String>>, id: String){
-        //gt_pt_dlt_obj_name.lock().unwrap().remove(id);
-        gt_pt_dlt_obj_name.lock().unwrap().entry(id);
-        let k = 2.to_string();
-        let binding = gt_pt_dlt_obj_name.lock().unwrap();
-        let data = binding.get(&k);
-        println!("{:?}", data);
-    }
-
+   
     let obj_type: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(HashMap::new()));
-    let add_obj_type = obj_type.clone();
-    thread::spawn(move || new_type(&add_obj_type));
-    fn new_type(new_type: &Mutex<HashMap<String, String>>){
-        new_type.lock().unwrap().insert(1.to_string(),"AP".to_string());
-        new_type.lock().unwrap().insert(2.to_string(),"DS".to_string());
-        new_type.lock().unwrap().insert(3.to_string(),"FU".to_string());
-    }
+    obj_type.lock().unwrap().insert(1.to_string(),"AP".to_string());
+    obj_type.lock().unwrap().insert(2.to_string(),"DS".to_string());
+    obj_type.lock().unwrap().insert(3.to_string(),"FU".to_string());
+    
+    let put_obj_type = obj_type.clone();
 
     match rq_tp.as_str() {
         "get" => {
@@ -64,13 +66,17 @@ fn handle_client(mut stream: TcpStream) {
             //let response = "Sent get to client";
             //stream.write(response.as_bytes()).unwrap();
         },
-        "put" => {
-            //сгенерить рандомный id для новой записи
+        "put" => {           
             let new_id = rand::thread_rng().gen_range(1..=100).to_string();
+            let new_id_nm = new_id.to_string();
+            let new_id_tp = new_id.to_string();
+            
             let nm = rquest[1].to_string();
-            let tp = rquest.get(2);
-            println!("Receive put");
-            put_name(&put_obj_name, new_id, nm);
+            let tp = rquest[2].to_string();
+            
+            put_obj_name.lock().unwrap().insert(new_id_nm, nm);
+            put_obj_type.lock().unwrap().insert(new_id_tp, tp);
+            //put_name(&put_obj_name, new_id, nm);
             //thread::spawn(move || new_name(&add_obj_name, tp));
             
         },
@@ -80,6 +86,18 @@ fn handle_client(mut stream: TcpStream) {
             //thread::spawn(move || dlt_name(&dlt_obj_name, &id));
             dlt_name(&dlt_obj_name, &id);
         },
+        /*"gen"=>{
+            let inj_code1:[&str; 16] = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+            let inj_code2 = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+            let mut new_code:String;
+            for char1 in 0..16 {
+                for char2 in 0..16{
+                    new_code = inj_code1[char1].to_string() + &inj_code2[char2].to_string();
+                    println!("{}", new_code)
+                };
+            };
+
+        }, */
         _ => println!("Receive X3"),
     }
     /*/*.map(|result| result.unwrap())
